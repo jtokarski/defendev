255b6bbb5dc9b2c8e0f12834be0e5923fb7e6b4f
Jozef Tokarski <jozef.tokarski@gmail.com>
2023-02-04 21:35:27 +0100

    Polishing abstract FixturesLoader

--- common-fixtures-library/src/main/java/org/defendev/common/fixture/loader/FixturesLoader.java
+++ common-fixtures-library/src/main/java/org/defendev/common/fixture/loader/FixturesLoader.java
@@ -1,5 +1,7 @@
 package org.defendev.common.fixture.loader;
 
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import org.defendev.common.domain.HasId;
 import org.defendev.common.fixture.loader.exception.DuplicateHardcodedIdException;
 import org.defendev.common.fixture.loader.exception.MissingActualIdForHardcodedIdException;
@@ -8,13 +10,12 @@ import org.defendev.common.fixture.loader.exception.UnableToLoadByActualIdExcept
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.isNull;
 
@@ -30,21 +31,25 @@ public abstract class FixturesLoader<E extends HasId<ID>, ID extends Serializabl
 
     protected Map<ID, ID> hardcodedToActualIds = new HashMap<>();
 
-    public FixturesLoader(SessionFactory sessionFactory, Class<E> entityClazz) throws JAXBException {
+    protected final String resourcePath;
+
+    public FixturesLoader(String resourcePath, SessionFactory sessionFactory, Class<E> entityClazz)
+        throws JAXBException
+    {
         this.entityClazz = entityClazz;
         this.sessionFactory = sessionFactory;
+        this.resourcePath = resourcePath;
         unmarshaller = setUpUnmarshaller();
     }
 
-    protected InputStream getResourceInputStream(String resourceName) {
-        final ClassLoader classLoader = Thread.currentThread()
-            .getContextClassLoader();
-        return classLoader.getResourceAsStream(resourceName);
+    protected InputStream getResourceInputStream(String resourcePath) {
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResourceAsStream(resourcePath);
     }
 
     public abstract Unmarshaller setUpUnmarshaller() throws JAXBException;
 
-    public abstract void readFromResourceAndPersist(String resourceName) throws JAXBException;
+    public abstract void readFromResourceAndPersist() throws JAXBException;
 
     public ID save(E newEntity) {
         final ID hardcodedId = newEntity.getId();
@@ -67,8 +72,8 @@ public abstract class FixturesLoader<E extends HasId<ID>, ID extends Serializabl
         return actualId;
     }
 
-    // for other Fixtures loaders to use:
-    public E loadByHardcodedId(ID hardcodedId) {
+    // To be used by other Fixture-loaders (for related entities resolution):
+    public E loadOneByHardcodedId(ID hardcodedId) {
         final ID actualId = hardcodedToActualIds.get(hardcodedId);
         if (isNull(actualId)) {
             throw new MissingActualIdForHardcodedIdException(entityClazz, hardcodedId);
@@ -81,18 +86,18 @@ public abstract class FixturesLoader<E extends HasId<ID>, ID extends Serializabl
         return loadedEntity;
     }
 
-    public List<E> loadByHardcodedId(List<ID> hardcodedIds) {
-        // TODO:
-        return List.of();
+    // To be used by other Fixture-loaders (for related entities resolution):
+    public Set<E> loadSetByHardcodedIds(Set<E> entitiesWithHardcodedIds) {
+        if (isNull(entitiesWithHardcodedIds) || entitiesWithHardcodedIds.isEmpty()) {
+            return Set.of();
+        }
+        return entitiesWithHardcodedIds.stream().map(
+            (E entity) -> {
+                final ID hardcodedId = entity.getId();
+                final E entityLoaded = loadOneByHardcodedId(hardcodedId);
+                return entityLoaded;
+            }
+        ).collect(Collectors.toSet());
     }
-
-//
-//  Todo: Is this method still needed?
-//
-//    public Integer getActualId(Integer hardcodedId) {
-//        return hardcodedToActualIds.get(hardcodedId);
-//    }
-
-
 
 }
