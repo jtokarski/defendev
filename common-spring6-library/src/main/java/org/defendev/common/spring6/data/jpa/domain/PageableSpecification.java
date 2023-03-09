090378c63cce309fdc5bbc2708af8b42a9ef2a27
Jozef Tokarski <jozef.tokarski@gmail.com>
2023-03-09 22:03:20 +0100

    Refactor PageableSpecification to abstract class instead of interface

--- common-spring6-library/src/main/java/org/defendev/common/spring6/data/jpa/domain/PageableSpecification.java
+++ common-spring6-library/src/main/java/org/defendev/common/spring6/data/jpa/domain/PageableSpecification.java
@@ -1,11 +1,13 @@
 package org.defendev.common.spring6.data.jpa.domain;
 
+import org.defendev.common.domain.query.Query;
 import org.defendev.common.domain.query.sort.SortDirection;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.JpaSort;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,26 +16,31 @@ import static java.util.Objects.isNull;
 
 
 
-public interface PageableSpecification<T> {
+public abstract class PageableSpecification<T extends Query> {
 
-    Map<SortDirection, Direction> TO_SPRINGDATA_DIRECTION = Map.of(
+    public static final Map<SortDirection, Direction> TO_SPRINGDATA_DIRECTION = Map.of(
         SortDirection.asc, Direction.ASC,
         SortDirection.desc, Direction.DESC
     );
 
-    static Sort jpaSortConjunct(List<JpaSort> sorts) {
-        if (isNull(sorts) || sorts.isEmpty()) {
-            throw new IllegalStateException("In order to conjunct, sorts list must not be empty");
-        }
-        Sort result = sorts.get(0);
-        for (int i = 1; i < sorts.size(); i += 1) {
-            result = result.and(sorts.get(i));
+    protected final T query;
+
+    public PageableSpecification(T query) {
+        this.query = query;
+    }
+
+    public abstract JpaSort getDefaultSort();
+
+    public abstract Pageable toPageable();
+
+    public Sort conjunctWithDefaultSort(List<JpaSort> sorts) {
+        final List<JpaSort> sortsWithDefault = new ArrayList<>(isNull(sorts) ? List.of() : sorts);
+        sortsWithDefault.add(getDefaultSort());
+        Sort result = sortsWithDefault.get(0);
+        for (int i = 1; i < sortsWithDefault.size(); i += 1) {
+            result = result.and(sortsWithDefault.get(i));
         }
         return result;
     }
-
-    JpaSort getDefaultSort();
-
-    Pageable toPageable();
 
 }
