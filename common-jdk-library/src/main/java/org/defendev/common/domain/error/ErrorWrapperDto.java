f4538a7ac4b0312c760bf9ed72fa657eea8a13b5
Jozef Tokarski <jozef.tokarski@gmail.com>
2023-07-17 13:15:04 +0200

    ErrorDto

--- common-jdk-library/src/main/java/org/defendev/common/domain/error/ErrorWrapperDto.java
+++ common-jdk-library/src/main/java/org/defendev/common/domain/error/ErrorWrapperDto.java
@@ -1,7 +1,9 @@
 package org.defendev.common.domain.error;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 
@@ -10,12 +12,30 @@ public class ErrorWrapperDto {
     private final List<ErrorDto> errors;
 
     public ErrorWrapperDto(ErrorDto primaryError) {
-        errors = new ArrayList<ErrorDto>();
-        errors.add(primaryError);
+        errors = List.of(primaryError);
     }
 
     public ErrorWrapperDto(List<ErrorDto> errors) {
         this.errors = errors;
+    }
+
+    public ErrorWrapperDto append(ErrorWrapperDto wrapperDto) {
+        final List<ErrorDto> allErrors = Stream.of(errors, wrapperDto.getErrors())
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+        return new ErrorWrapperDto(allErrors);
+    }
+
+    public ErrorWrapperDto append(ErrorDto errorDto) {
+        final List<ErrorDto> allErrors = Stream.concat(errors.stream(), Stream.of(errorDto))
+            .collect(Collectors.toList());
+        return new ErrorWrapperDto(allErrors);
+    }
+
+    public ErrorWrapperDto prepend(ErrorDto errorDto) {
+        final List<ErrorDto> allErrors = Stream.concat(Stream.of(errorDto), errors.stream())
+            .collect(Collectors.toList());
+        return new ErrorWrapperDto(allErrors);
     }
 
     public List<ErrorDto> getErrors() {
