dbf6292f386288e695a65c22eab9f00314a956e6
Jozef Tokarski <jozef.tokarski@gmail.com>
2024-04-16 22:20:24 +0200

    Release common-jdk 0.0.10

--- common-jdk-library/src/main/java/org/defendev/common/stream/Streams.java
+++ common-jdk-library/src/main/java/org/defendev/common/stream/Streams.java
@@ -1,7 +1,9 @@
 package org.defendev.common.stream;
 
-import java.util.stream.Stream;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.stream.Stream;
 
 import static java.util.Objects.isNull;
 
@@ -13,6 +15,12 @@ public class Streams {
         return isNull(collection) ?
             Stream.<E>empty()
             : collection.stream();
+    }
+
+    public static <E> Stream<E> stream(Enumeration<E> enumeration) {
+        return isNull(enumeration) ?
+            Stream.<E>empty()
+            : Collections.list(enumeration).stream();
     }
 
     /* TODO: possibly consider variant with Iterable  */
